c375ec173014c9535330fbb72423448daae16dbf
Sebbie Mzing <Sebbie Evayo>
2024-10-16 15:26:06 +0300

    create api

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for,flash, redirect
+from flask import Flask, render_template, url_for,flash, redirect, request
 from forms import RegistrationForm, LoginForm
 import email_validator
+import pandas as pd
+import joblib
+import xgboost
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='a5cd36c715058bf2c9057169b7134a4d'
@@ -15,7 +18,7 @@ def login():
         return redirect(url_for('hello_world'))
     return render_template('login.html', title='Login', form=form)
 
-@app.route("/home")
+@app.route("/home",methods=["GET","POST"])
 def hello_world():
     return render_template('home.html')
 
@@ -32,7 +35,32 @@ def register():
         return redirect(url_for('hello_world'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route("/predict", methods = ["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
 
+        try:
+            prediction = preprocessDataAndPredict(to_predict_list)
+            return render_template('/predict.html', prediction = prediction)
+        
+        except ValueError:
+            return "Please Enter Valid Values"
+        
+
+    return "Method not allowed"
+
+def preprocessDataAndPredict(feature_dict):
+    test_data = {k:[v] for k, v in feature_dict.items()}
+    test_data = pd.DataFrame(test_data) 
+    
+    file = open("safe_mom_model_1.pkl", "rb")
+    
+    trained_model = joblib.load(file)
+    
+    predict = trained_model.predict(test_data)
+
+    return predict
 
 
 if __name__ == '__main__':
